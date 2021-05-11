99f0d8584a7aa4b008f13765486a68f3bf45f41d
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-11 10:43:41 +0000

    [Add step3 仕上げ　残りユーザプロフィール作成]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20}
-  # validates :introduction, presence: true, length: { maximum: 50}
+  validates :introduction, presence: true, length: { maximum: 50}
 
   has_many :books, dependent: :destroy
   attachment :profile_image
