f91d91760f1771f22af4081d1e57d7cdb5c5278c
kinoshitaken123 <k1k7s9e3@gmail.com>
2021-05-25 07:48:05 +0000

    [開発スキルパッチ処理]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  config.paths.add'lib',eager_load:true
 
   before_action :authenticate_user!,except: [:top, :about, :public_action]
   before_action :configure_permitted_parameters, if: :devise_controller?
